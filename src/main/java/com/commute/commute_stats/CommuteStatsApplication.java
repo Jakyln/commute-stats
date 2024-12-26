a1b8acf7f8604f5294af8238f259a706961dd78f
julien <kalaycijulien@gmail.com>
2024-12-26 16:53:06 +0100

    init

---
+++ src/main/java/com/commute/commute_stats/CommuteStatsApplication.java
@@ -0,0 +1,13 @@
+package com.commute.commute_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuteStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commute/commute_stats/CommuteStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commute.commute_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuteStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
